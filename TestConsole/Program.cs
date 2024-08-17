34014ac0c405fbde4ec94317d4d4baa2e5981942
Wayne Michael <WayneM@hollywoodbets.net>
2024-08-17 14:00:08 +0200

    added customised greeting functionality

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -1,3 +1,4 @@
-﻿Console.WriteLine("Hello, World!");
-Console.WriteLine("It's so fun working in a git repository.");
-Console.WriteLine("This is another message to the console.");
+﻿// get user name from the command prompt and print customised 
+Console.Write("Please enter your name: ");
+string name = Console.ReadLine() ?? string.Empty;
+Console.WriteLine($"Hello {name}, welcome!");
