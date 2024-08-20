43ec49b451e0f1db671f7c1c2384da7bb1cc7b08
Wayne Michael <WayneM@hollywoodbets.net>
2024-08-20 20:19:28 +0200

    added logging to textfile functionality

--- TestConsole/Settings/Paths.cs
+++ TestConsole/Settings/Paths.cs
@@ -6,4 +6,5 @@ public static class Paths
 {
     public static readonly string DummyDataPath = @"c:\TestApps\TestRepo\TestConsole\Data\";
     public static readonly string DummyDataFile = "dummydata.txt";
+    public static readonly string LogPath = @"c:\TestApps\TestRepo\TestConsole\Log\";
 }
