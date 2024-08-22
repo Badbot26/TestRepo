1f5f6f2382b9b1c2478100b9d2aa1e6b827dbd47
Wayne Michael <WayneM@hollywoodbets.net>
2024-08-22 16:45:39 +0200

    created a long running logger method to run synchronously

---
+++ AsyncApp/Program.cs
@@ -0,0 +1,40 @@
+﻿static void SimulateLongRunningTask(int seconds)
+{
+    Thread.Sleep(seconds * 1000);
+    
+    // log to text file and console to show when execution in this method resumes
+    string logMessage = DateTime.Now.ToString() + ": logged" + Environment.NewLine;
+    Console.Write(logMessage);
+    File.AppendAllText("c:/users/waynem/desktop/log.txt", logMessage);
+}
+
+static string LogData()
+{
+    Console.WriteLine("entered LogData()");
+    try
+    {
+        // simulate long running logging
+        SimulateLongRunningTask(seconds: 3);
+    }
+    catch
+    {
+        return "unsuccessful";
+    }
+    Console.WriteLine("leaving LogData()");
+        return "success";
+}
+
+static string UseLogData()
+{
+    Console.WriteLine("entered UseLogData()");
+    var logDataResponse = LogData();
+    Console.WriteLine("leaving UseLogData()");
+    return logDataResponse;
+}
+
+Console.WriteLine("entered program");
+var useLogdataResponse = UseLogData();
+Console.WriteLine("response: " + useLogdataResponse);
+Console.WriteLine("leaving program");
+
+Console.ReadLine();
