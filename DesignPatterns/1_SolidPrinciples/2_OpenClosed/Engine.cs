1d118a8e25927c29e3c507ca3c2861dc7cf2e0ba
Wayne Michael <pwaynemichael@gmail.com>
2024-10-06 22:16:12 +0200

    feat: added open-closed principle setup code and program, project and solution files

---
+++ DesignPatterns/1_SolidPrinciples/2_OpenClosed/Engine.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.SolidPrinciples.OpenClosed
+{
+    public static class Engine
+    {
+        public static void Run()
+        {
+            Console.WriteLine("Running the Open-Closed Principle Engine");
+
+            var apple = new Product("Apple", Color.Green, Size.Small);
+            var tree = new Product("Tree", Color.Green, Size.Large);
+            var house = new Product("House", Color.Blue, Size.Large);
+
+            Product[] products = [apple, tree, house];
+            var pf = new ProductFilter();
+            Console.WriteLine("Green products (old):");
+            foreach (var p in pf.FilterByColor(products, Color.Green))
+            {
+                Console.WriteLine($" - {p.Name} is green");
+            }
+        }
+
+        public enum Color
+        {
+            Red, Green, Blue
+        }
+
+        public enum Size
+        {
+            Small, Medium, Large, Huge
+        }
+
+        public class Product
+        {
+            public string Name;
+            public Color Color;
+            public Size Size;
+
+            public Product(string name, Color color, Size size)
+            {
+                if (name == null)
+                    throw new ArgumentNullException("name");
+
+                Name = name;
+                Color = color;
+                Size = size;
+            }
+        }
+
+        public class ProductFilter
+        {
+            public IEnumerable<Product> FilterBySize(IEnumerable<Product> products, Size size)
+            {
+                foreach (Product product in products)
+                    if (product.Size == size)
+                        yield return product;
+            }
+
+            public IEnumerable<Product> FilterByColor(IEnumerable<Product> products, Color color)
+            {
+                foreach (Product product in products)
+                    if (product.Color == color)
+                        yield return product;
+            }
+
+            public IEnumerable<Product> FilterBySizeAndColor(IEnumerable<Product> products, Size size, Color color)
+            {
+                foreach (Product product in products)
+                    if (product.Size == size && product.Color == color)
+                        yield return product;
+            }
+        }
+
+        public interface ISpecification<T>
+        {
+            bool IsSatisfied(T t);
+        }
+    }
+}
---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,9 @@
+﻿// config
+var solidPrinciples_SingleResponsibilityPrinciple = false;
+var solidPrinciples_OpenClosedPrinciple = true;
+
+if (solidPrinciples_SingleResponsibilityPrinciple)
+    DesignPatterns.SolidPrinciples.SingleResponsibility.Engine.Run();
+
+if (solidPrinciples_OpenClosedPrinciple)
+    DesignPatterns.SolidPrinciples.OpenClosed.Engine.Run();
