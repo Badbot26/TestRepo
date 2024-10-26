04b6ce5ccf54d395fd4c72f10e909705fb84d4d2
Wayne Michael <pwaynemichael@gmail.com>
2024-10-26 21:06:02 +0200

    feat: completed the open closed principle.

--- DesignPatterns/1_SolidPrinciples/2_OpenClosed/Engine.cs
+++ DesignPatterns/1_SolidPrinciples/2_OpenClosed/Engine.cs
@@ -24,6 +24,24 @@ namespace DesignPatterns.SolidPrinciples.OpenClosed
             {
                 Console.WriteLine($" - {p.Name} is green");
             }
+
+            var bf = new BetterFilter();
+            Console.WriteLine("Green products (new):");
+            foreach (var p in bf.Filter(products, new ColorSpecification(Color.Green)))
+            {
+                Console.WriteLine($" - {p.Name} is green");
+            }
+
+            Console.WriteLine("Large blue items");
+            foreach (var p in bf.Filter(
+                products,
+                new AndSpecification<Product>(
+                    new ColorSpecification(Color.Blue),
+                    new SizeSpecification(Size.Large)
+                    )))
+            {
+                Console.WriteLine($" - {p.Name} is big and blue");
+            }
         }
 
         public enum Color
@@ -85,6 +103,62 @@ namespace DesignPatterns.SolidPrinciples.OpenClosed
         public interface IFilter<T>
         {
             IEnumerable<T> Filter(IEnumerable<T> items, ISpecification<T> spec);
+        }
+
+        public class ColorSpecification : ISpecification<Product>
+        {
+            private Color color;
+
+            public ColorSpecification(Color color)
+            {
+                this.color = color;
+            }
+
+            public bool IsSatisfied(Product t)
+            {
+                return t.Color == color;
+            }
+        }
+
+        public class SizeSpecification : ISpecification<Product>
+        {
+            private Size size;
+
+            public SizeSpecification(Size size)
+            {
+                this.size = size;
+            }
+
+            public bool IsSatisfied(Product t)
+            {
+                return t.Size == size;
+            }
+        }
+
+        public class AndSpecification<T> : ISpecification<T>
+        {
+            private ISpecification<T> first, second;
+
+            public AndSpecification(ISpecification<T> first, ISpecification<T> second)
+            {
+                this.first = first ?? throw new ArgumentNullException(nameof(first));
+                this.second = second ?? throw new ArgumentNullException(nameof(second));
+            }
+
+            public bool IsSatisfied(T t)
+            {
+                return first.IsSatisfied(t) && second.IsSatisfied(t);
+            }
+        }
+
+        public class BetterFilter : IFilter<Product>
+        {
+            public IEnumerable<Product> Filter(IEnumerable<Product> items, ISpecification<Product> spec)
+            {
+                foreach (var i in items)
+                    if (spec.IsSatisfied(i))
+                        yield return i;
+            }
         }
     }
 }
